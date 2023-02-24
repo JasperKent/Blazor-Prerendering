ae8f3df60a3d3a0c74d12146ed9d917dc95bd1d8
Jasper Kent <software@jasperkent.com>
2023-02-24 08:17:38 +0000

    Add project files.

---
+++ ServerSideStartup/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using ServerSideStartup.Shared;
+
+namespace ServerSideStartup.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            await Task.Delay(2000);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
